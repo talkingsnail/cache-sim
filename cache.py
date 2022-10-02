5f385872441cdc206767621bf7d361c2c7ac2129
talkingsnail <114215262+talkingsnail@users.noreply.github.com>
2022-10-02 20:59:52 +0800

    Add cache replacement policies

--- cache.py
+++ cache.py
@@ -1,3 +1,12 @@
+import enum
+
+class ReplacementPolicy(enum.Enum):
+    LRU = enum.auto()
+    MRU = enum.auto()
+    FIFO = enum.auto()
+    RANDOM = enum.auto()
+
+    
 class Cache:
     def __init__(self):
         self.size = 16
